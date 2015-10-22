171856d1d653242b2f7117e0c0f35b3720218000
姜宏宇 <jianghongyu@jianghongyudeMacBook-Pro.local>
2015-10-22 20:03:39 +0800

    第一次

---
+++ demo/demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo
+//
+//  Created by 姜宏宇 on 15/10/22.
+//  Copyright © 2015年 姜宏宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demo/demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  demo
+//
+//  Created by 姜宏宇 on 15/10/22.
+//  Copyright © 2015年 姜宏宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
